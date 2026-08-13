8cae9e2ca81a15d9cba23a016377df79c2959763
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-13 16:25:21 +0530

    Learn Update query in SQL

---
+++ updatingData.sql
@@ -0,0 +1,8 @@
+select * from users;
+
+UPDATE users SET salary=50000 where id =1 ;
+
+Update users SET name='SRV', email='SRV@gmail.com' where id = 2;
+update users SET name='Raj Shamani', email='rajshamani@gmail.com' where id = 3 ;
+UPDATE users SET name = 'Vivek', email='vivek@gmail.com' WHERE id = 4;
+UPDATE users SET name='Ashish' where id = 30;
