a236c41abc6e9f5a636f9b2ec71932c8ef65a67c
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-13 16:48:17 +0530

    Learn Update query in SQL

--- updatingData.sql
+++ updatingData.sql
@@ -7,4 +7,6 @@ update users SET name='Raj Shamani', email='rajshamani@gmail.com' where id = 3 ;
 UPDATE users SET name = 'Vivek', email='vivek@gmail.com' WHERE id = 4;
 UPDATE users SET name='Ashish' where id = 30;
 
+UPDATE users SET salary = salary+10000 where salary<60000;
+
 UPDATE users SET name='Aisha Khan' where email='aisha@example.com';
