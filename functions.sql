c836c818c314f8c8eae27e24d5946dfd1134162a
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-22 16:25:40 +0530

    Learn string methods and sum and avg

--- functions.sql
+++ functions.sql
@@ -1,7 +1,10 @@
+--to count how many users were there !
+
 select count(*) from users;
 
 select * from users;
 
+--to count how many male and female users were there !
 select count(*) from users where gender='male';
 select count(*) from users where gender='female';
 
@@ -21,3 +24,19 @@ SELECT MIN(salary) AS minSalary, MAX(salary) AS maxSalary from users;
 Select id,name,salary from users 
 where salary=(SELECT MIN(salary) from users) 
 or salary=(SELECT MAX(salary) from users);
+
+SELECT sum(salary) AS totalSalary from users;
+
+select avg(salary) AS avgSalary from users;
+
+SELECT gender, sum(salary) as total from users GROUP BY gender; 
+
+SELECT gender , avg(salary) as avg_total from users GROUP BY gender;
+
+select sum(salary ) from users where gender = 'male';
+select sum(salary) from users where gender = 'female';
+
+select sum(salary) from users where salary > 50000;
+
+select id, gender, Lower(name), CONCAT(lower(name),6712) AS username, length(name) AS len from users;
+
