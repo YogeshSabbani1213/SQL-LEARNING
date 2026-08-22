7371ba5327fcd0ccb225958428ba030fd09e048a
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-22 14:30:38 +0530

    Learn functions MIN and MAX

--- functions.sql
+++ functions.sql
@@ -9,3 +9,15 @@ select count(*) from users where salary>60000 and gender = 'male';
 select count(*) from users where dateOfBirth is NULL;
 
 select * from users
+
+
+SELECT MIN(salary) AS minSalary, MAX(salary) AS maxSalary from users;
+
+
+--If you want the name, id, and age of the users who
+-- have the minimum and maximum salary, 
+--you cannot simply add name, id, age with MIN() and MAX().
+
+Select id,name,salary from users 
+where salary=(SELECT MIN(salary) from users) 
+or salary=(SELECT MAX(salary) from users);
