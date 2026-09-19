276344e6efdccbaccdafb96af5695d68533f200f
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-19 17:29:21 +0530

    Learn foreign key

--- foriegnKey.sql
+++ foriegnKey.sql
@@ -3,6 +3,8 @@ drop table IF EXISTS adressess;
 create table adressess (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    
+    street VARCHAR(255),
+    city VARCHAR(255),
+   
     constraint fk_user FOREIGN KEY(user_id) REFERENCES users(id)
 )
