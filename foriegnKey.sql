a8099a79f7861733d3f444a0c9e1632acc23faa4
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-19 17:29:40 +0530

    Revise foreign key

--- foriegnKey.sql
+++ foriegnKey.sql
@@ -5,6 +5,7 @@ create table adressess (
     user_id INT,
     street VARCHAR(255),
     city VARCHAR(255),
-   
+    state VARCHAR,
+    pincode VARCHAR,
     constraint fk_user FOREIGN KEY(user_id) REFERENCES users(id)
 )
