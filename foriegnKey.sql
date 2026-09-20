fd3f7a192e83c2d764680f7aef196481c5c19ccf
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-20 16:49:01 +0530

    Insert values

--- foriegnKey.sql
+++ foriegnKey.sql
@@ -10,3 +10,10 @@ CREATE TABLE addresses(
     CONSTRAINT fk_key FOREIGN KEY(user_id) REFERENCES users(id) on DELETE CASCADE
 );
 
+--We create id as the primary key to uniquely identify each address record, and user_id as the foreign key to link that address to a specific user in the users table.
+--`ON DELETE CASCADE` automatically deletes all related child records when the corresponding parent record is deleted.
+
+
+INSERT INTO addresses (id,user_id,city,state) VALUES (1,1,"gujarat","UP");
+
+select * from addresses;
