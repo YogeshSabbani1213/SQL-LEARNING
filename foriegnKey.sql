34326548f77725c2676c0c1121c9e8fea5e4b5d8
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-20 16:38:48 +0530

    REvise foreign key

--- foriegnKey.sql
+++ foriegnKey.sql
@@ -1,14 +1,12 @@
-drop table IF EXISTS adressess;
-create table adressess (
+drop table if EXISTS adresses;
+
+show Tables;
+
+CREATE TABLE addresses(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    street VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(30),
-    pincode VARCHAR(10),
-    constraint fk_user FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    user_id INT ,
+    city VARCHAR(200),
+    state VARCHAR(200),
+    CONSTRAINT fk_key FOREIGN KEY(user_id) REFERENCES users(id) on DELETE CASCADE
 );
 
-select * from users;
-
-show tables;
