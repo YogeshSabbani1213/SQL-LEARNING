8a66f9c258504fee1e32b70eea5ee1cf8493447d
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-21 20:19:35 +0530

    Practice InnerJoin

--- innerJoin.sql
+++ innerJoin.sql
@@ -11,4 +11,11 @@ INNER JOIN addresses ON users.id = addresses.user_id
 --AS is used to give a column or table a temporary alias 
 --(new name) in the query result.
 
+--Display user ID, name, city, and state.
+SELECT users.id,users.name,addresses.city,addresses.state from users
+INNER JOIN addresses ON users.id = addresses.user_id;
 
+--Display only users whose city is Hyderabad.
+SELECT users.name , addresses.city from users 
+INNER JOIN addresses ON users.id = addresses.user_id
+WHERE addresses.city='Hyderabad';
