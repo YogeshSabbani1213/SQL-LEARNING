4fb67ba6f8018e8e1573235ba318464a39a2d748
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-22 11:59:13 +0530

    Practice LeftJoin

--- leftJoin.sql
+++ leftJoin.sql
@@ -4,3 +4,13 @@ LEFT JOIN addresses ON users.id = addresses.user_id;
 SELECT * FROM users;
 
 SELECT * FROM addresses;
+
+--Display all users even if they don't have an address.
+SELECT users.id,users.name,addresses.city from users
+LEFT JOIN addresses ON users.id = addresses.user_id;
+
+--Find users who do NOT have an address.
+SELECT users.name from users
+LEFT JOIN addresses ON users.id=addresses.user_id
+WHERE addresses.user_id IS NULL;
+
