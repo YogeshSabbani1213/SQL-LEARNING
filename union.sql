341c55d68f33b9064c44b7960877bd2e68d5ce41
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-22 15:43:38 +0530

    Revise Union

--- union.sql
+++ union.sql
@@ -2,10 +2,11 @@ SELECT id,name from users
 UNION
 SELECT id,name from admin_table;
 
-SELECT id,name, 
+SELECT id,name, dateOfBirth,
 'User' AS role from users
 UNION
-SELECT id,name, 
-'Admin' AS role from admin_table;
+SELECT id,name, dateOfBirth,
+'Admin' AS role from admin_table
+ORDER BY dateOfBirth;
 
 SHOW TABLES;
